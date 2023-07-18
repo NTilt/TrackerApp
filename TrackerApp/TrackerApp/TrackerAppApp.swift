0da0143998414bd90896bff6afb410bd1ee487f8
Nikita <ya.n.2@mail.com>
2023-07-18 17:51:50 +0400

    initial commit

---
+++ TrackerApp/TrackerApp/TrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerAppApp.swift
+//  TrackerApp
+//
+//  Created by Никита Ясеник on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
